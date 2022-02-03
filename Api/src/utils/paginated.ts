4ce20a44cc1a8077f672119d42a72a6904764e0b
Maximiliano Casanova <maxi.abel4002@gmail.com>
2022-02-03 14:23:21 -0300

    Add Paginated Function, refactor /Directory route, minor change to Sort Function

---
+++ Api/src/utils/paginated.ts
@@ -0,0 +1,21 @@
+import { db } from "../app";
+import Manga from '../classes/Manga';
+
+export default async function paginated (numPaged:number): Promise<[Manga[], number]> {
+    let mangas: Manga[] = [];
+    let mangasPerPage: number = 25;
+    let totalMangas: number = await db.manga.count();
+    let totalPages: number = Math.ceil(totalMangas / mangasPerPage);
+    let page: number = numPaged;
+    if(page > totalPages) throw new Error("Page not found");
+    let offset: number = (page - 1) * mangasPerPage;
+    let mangasPaginated: Manga[] = await db.manga.findMany({
+        take: mangasPerPage,
+        skip: offset,
+        orderBy: {
+            rating: "desc"
+        }
+    });
+    mangas = mangasPaginated;
+    return [mangas, totalPages];
+}
