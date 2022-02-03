b3a61f9b177c28f117b0007d3daf32d12052256e
Maximiliano Casanova <maxi.abel4002@gmail.com>
2022-02-03 16:34:52 -0300

    Fixed Cors

--- Api/src/app.ts
+++ Api/src/app.ts
@@ -2,10 +2,22 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import { PrismaClient } from '@prisma/client';
 import { routes } from './routes/index'
-
+import cors from "cors";
 export const db = new PrismaClient();
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+// server.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());
