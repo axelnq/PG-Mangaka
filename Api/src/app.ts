e793438bab02d09fbacb522d9e0e0b7d2fc9c778
Rutito2010 <kakaroto11@live.com.ar>
2022-02-04 10:20:45 -0300

    nombre de autor en los mangas

--- Api/src/app.ts
+++ Api/src/app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
-import { PrismaClient } from '@prisma/client';
-import { routes } from './routes/index'
+import { PrismaClient } from "@prisma/client";
+import { routes } from "./routes/index";
 import cors from "cors";
 export const db = new PrismaClient();
 
@@ -21,19 +21,17 @@ app.use((req, res, next) => {
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use('/api', routes);
-
-
+app.use("/api", routes);
 
 // Error catching endware.
-app.use((err: any, req: Request, res: Response, next: NextFunction) => { // eslint-disable-line no-unused-vars
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send({ message });
 });
 
-
 app.listen(3001, () => {
-  console.log('Server listening on port 3001');
-})
+  console.log("Server listening on port 3001");
+});
--- Api/src/utils/paginated.ts
+++ Api/src/utils/paginated.ts
@@ -1,21 +1,30 @@
 import { db } from "../app";
-import Manga from '../classes/Manga';
+import Manga from "../classes/Manga";
 
-export default async function paginated (numPaged:number): Promise<[Manga[], number]> {
-    let mangas: Manga[] = [];
-    let mangasPerPage: number = 25;
-    let totalMangas: number = await db.manga.count();
-    let totalPages: number = Math.ceil(totalMangas / mangasPerPage);
-    let page: number = numPaged;
-    if(page > totalPages) throw new Error("Page not found");
-    let offset: number = (page - 1) * mangasPerPage;
-    let mangasPaginated: Manga[] = await db.manga.findMany({
-        take: mangasPerPage,
-        skip: offset,
-        orderBy: {
-            rating: "desc"
-        }
-    });
-    mangas = mangasPaginated;
-    return [mangas, totalPages];
+export default async function paginated(
+  numPaged: number
+): Promise<[Manga[], number]> {
+  let mangas: Manga[] = [];
+  let mangasPerPage: number = 25;
+  let totalMangas: number = await db.manga.count();
+  let totalPages: number = Math.ceil(totalMangas / mangasPerPage);
+  let page: number = numPaged;
+  if (page > totalPages) throw new Error("Page not found");
+  let offset: number = (page - 1) * mangasPerPage;
+  let mangasPaginated: Manga[] = await db.manga.findMany({
+    take: mangasPerPage,
+    skip: offset,
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      rating: "desc",
+    },
+  });
+  mangas = mangasPaginated;
+  return [mangas, totalPages];
 }
