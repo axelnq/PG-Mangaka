da047f3efc5b38fefece0b58553cceb5d482257d
yas lillo <yasnalillo27@gmail.com>
2022-02-05 18:04:25 -0300

    ../client/src/Components/CreateForm.jsx

--- client/src/Components/CreateForm.jsx
+++ client/src/Components/CreateForm.jsx
@@ -1,82 +1,80 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { FormControl } from '@mui/material';
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { postManga, FILTRO_GENERO, filterMangasByGenre, getAllMangas } from '../Actions/index';
+import { postManga, getAllMangas } from '../Actions/index';
 
 
 export default function CreateForm() {
-  // const dispatch = useDispatch();
-  // const history = useNavigate();
-  // const generos = useSelector((state) => state.allMangas);
+  const dispatch = useDispatch();
+  const generos = useSelector((state) => state.allMangas);
 
   const [input, setInput] = useState({
     title: '',
     synopsis: '',
-    image: '',
-    genre: [],
+    images: [],
+    genres: [],
+  
 
-  })
+  });
 
-  // function handleChange(e){
-  //   setInput({
-  //     ...input,
-  //     [e.target.name]: e.target.value,
-  //   });
+  function handleChange(e){
+    console.log(e.target.value)
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    }); 
+  }
+  
+  function handleSelect(e) {
+    console.log(e.target.value)
+    setInput({
+        ...input,
+        genres: [...input.genres, e.target.value],
 
-  //   function handleSelect(e) {
-  //     setInput({
-  //         ...input,
-  //         genre: [...input.genre, e.target.value],
-  //     });
-  // }
+    });
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    const {title, synopsis,images,genres} = input;
+    
+    if (title === undefined || title.length < 3) {
+      return alert ('titulo invalido')
+    } else if(synopsis === undefined || synopsis.length < 30) {
+      return alert ('synpsis minima 30 caracteres')
+    } else if (images === undefined) {
+      return alert('ingrese imagen valida')
+    } else if (genres === undefined) {
+      return alert('seleccione genero')
+    }
 
-  // function handleSubmit(e) {
-  //   e.preventDefault();
+    //id de autor jarcodeado para poder crear manga,cambiar el id cada vez que se cree una nueva manga"
+    input.authorId =  "f93e8fcd-fc26-44b4-9999-01ea6935854b"
+    dispatch(postManga(input));
+    alert('Manga creada');
+    setInput({
+      title:'',
+      synopsis:'',
+      images:[],
+      genres:[],
+    });
 
-  //   const {title, synopsis,image,genre} = input;
-
-  //   if(title === undefined || title.length < 3) {
-  //     return alert ('titulo invalido')
-  //   }else if(synopsis === undefined || synopsis.length < 30) {
-  //     return alert ('synpsis minima 30 caracteres')
-  //   }else if (image === undefined) {
-  //     return alert('ingrese imagen valida')
-  //   }else if (genre === undefined) {
-  //     return alert('seleccione genero')
-  //   }
-  // }
-
-  // dispatch(postManga(input));
-  // alert('Manga creada');
-  // setInput({
-  //   title:'',
-  //   synopsis:'',
-  //   image:'',
-  //   genre:[],
-  // });
-  // history.push('/home')
-
-
-  //  }
-
-  //   function handleDelete(el) {
-  //     setInput({
-  //         ...input,
-  //         genre: input.genre.filter((generos) => generos !== el),
-  //     });
-  // }
-
-  // useEffect(() =>{
-  //   dispatch(filterMangasByGenre());
-  // },[dispatch])
-
-
-
-
+  }
+  function handleDelete(el) {
+    setInput({
+      ...input,
+        genre: input.genre.filter((generos) => generos !== el),
+      });
+  }
+  
+  useEffect(() =>{
+    dispatch(getAllMangas());
+  },[dispatch])
 
   return (
     <Box
@@ -85,7 +83,7 @@ export default function CreateForm() {
       sx={{ mt: '15%' }}
       sx={{ md: { xs: '20%', md: '40%', lg: '100%' } }}>
       <div>
-        <FormControl
+        <FormControl onSubmit={(e) => handleSubmit(e)}
           sx={{
             width: 300,
             height: 400,
@@ -98,9 +96,9 @@ export default function CreateForm() {
           <div>
             <input
               type="text"
-              // value={input.title}
+              value={input.title}
               name="title"
-            // onChange={(e) => handleChange(e)}
+            onChange={(e) => handleChange(e)}
 
             />
           </div>
@@ -109,9 +107,9 @@ export default function CreateForm() {
             <div>
               <input
                 type="text"
-                // value={input.synopsis}
-                name="synposis"
-              // onChange={(e) => handleChange(e)}
+                value={input.synopsis}
+                name="synopsis"
+              onChange={(e) => handleChange(e)}
               />
 
             </div>
@@ -120,39 +118,40 @@ export default function CreateForm() {
             <label>IMAGEN :</label>
             <div>
               <input
-                type="text"
-                // value={input.image}
-                name="image"
-              // onChange={(e) => handleChange(e)}
+                type='text'
+                value={input.images}
+                name="images"
+              onChange={(e) => handleChange(e)}
               />
             </div>
           </Box>
           <Box sx={{ mt: '1rem' }}>
             <label>GENERO :</label>
             <div>
-              <select >
+              <select  onChange={(e) => handleSelect(e)}>
                 <option value="Action">Acción</option>
                 <option value="Adventure">Aventura</option>
                 <option value="Comedy">Comedia</option>
                 <option value="Drama">Drama</option>
+                <option value="Ecchi">Ecchi</option>
                 <option value="Fantasy">Fantasía</option>
+                <option value="Mistery">Misterio</option>
                 <option value="Romance">Romance</option>
-                {/* genre:
-                            {generos.map((el) => (
-                                <option key={el.id} value={el.name}>
-                                    {el.name}
-                                </option>
-                            ))} */}
+                <option value="Sci-Fi">Ciencia Ficción</option>
+                <option value="Slice of Life">Historia de Vida</option>
+                <option value="Supernatural">Sobrenatural</option>
+                <option value="Sports">Deporte</option>
               </select>
             </div>
           </Box>
           <div>
             <Box sx={{ width: '100%', py: '1rem' }}>
-              <Button type="submit" size="small" className='btnCreate' variant="contained">Crear Manga</Button></Box>
+              <Button onClick={(e) => handleSubmit(e)} size="small"  variant="contained">Crear Manga</Button></Box>
             <Box sx={{ width: '100%', py: '0.2rem' }}>
-              <Button size="small" variant="contained" href="/home">
-                Home
-              </Button>  </Box>
+              <NavLink to="/">
+              <Button>Home</Button>
+              </NavLink>
+              </Box>
           </div>
         </FormControl>
       </div>
