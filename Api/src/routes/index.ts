cc890b664193298d8facd6d571cf1652ab194772
Rutito2010 <kakaroto11@live.com.ar>
2022-02-10 14:59:04 -0300

    compra local de mangas y monedas

--- Api/src/routes/index.ts
+++ Api/src/routes/index.ts
@@ -3,10 +3,14 @@ import { usersRouter } from "./users";
 import { mangasRouter } from "./mangas";
 import { chaptersRouter } from "./chapters";
 import { authRouter } from "./auth";
+import { internalOrderRouter } from "./chapterBuy";
+import { externalOrderRouter } from "./coins";
 
 export const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/mangas", mangasRouter);
 routes.use("/chapters", chaptersRouter);
-routes.use("/auth", authRouter)
+routes.use("/auth", authRouter);
+routes.use("/buyChapter", internalOrderRouter);
+routes.use("/Coins", externalOrderRouter);
