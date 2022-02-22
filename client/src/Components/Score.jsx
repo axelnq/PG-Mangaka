b8ac548b24265376d73efd55a90e9e1d78b0fccd
Cintia Arce <cintiaayelenarce@gmail.com>
2022-02-22 11:49:37 -0300

    Score fixed

--- client/src/Components/Score.jsx
+++ client/src/Components/Score.jsx
@@ -4,20 +4,21 @@ import Rating from '@mui/material/Rating';
 
 const axios = require('axios')
 
-const Score = ({id}) => {
-    let [value, setValue] = React.useState(0)
-
-    let handleRating = () => {
-        axios.put(`http://localhost:3001/api/chapters/chapter/vote/${id}`, { withCredentials: true })
+const Score = ({id, chapter}) => {
+    let handleRating = (e) => {
+        axios.put(`http://localhost:3001/api/chapters/chapter/vote/${id}`,{ points: e.target.value }, { withCredentials: true })
+            .then(data => console.log(chapter.points))
+            .catch(error => console.log(error.response))
     }
 
     return (
-        <Box sx={{backgroundColor:'#fffff9', borderRadius: '10%'}}>
-            <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => { setValue(newValue) }}
-            />
+        <Box>
+            {
+                (chapter.points === 0) ?
+                <Rating onClick={handleRating}/>
+                :
+                <Rating name="read-only" value={chapter.points} readOnly />
+            }
         </Box>
     )
 }
