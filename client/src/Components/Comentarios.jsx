2a6f8f6fef39500b8166479428bb7c23e3ce53b8
LEONEL ESCUDERO <71057664+Leonani@users.noreply.github.com>
2022-02-23 18:52:42 -0300

    css commit
    estilo en indexProfile

--- client/src/Components/Comentarios.jsx
+++ client/src/Components/Comentarios.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, useParams} from "react-router-dom"
+import {useParams} from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
