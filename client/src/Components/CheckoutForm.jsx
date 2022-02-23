e7191203b30f9ace4ac0ff1247d493e98655404f
yas lillo <yasnalillo27@gmail.com>
2022-02-23 04:14:17 -0300

    feature: checkout form validations

--- client/src/Components/CheckoutForm.jsx
+++ client/src/Components/CheckoutForm.jsx
@@ -9,173 +9,165 @@ import { Button } from "@mui/material";
 import { Input} from "@mui/material";
 import { useNavigate } from 'react-router-dom';
 
+function validate(input) {
+  const error = {};
+  const { name, cbu, coins } = input;
+  
+  error.name = name.length > 3 && isNaN(name) ? null : 'Ingrese nombre del titular de la cuenta';  
+  error.cbu = cbu && cbu.length === 22 && !isNaN(cbu) ? null: 'El cbu debe tener 22 digitos';
+  error.coins = coins > 0 ? null : 'Las monedas a extraer deben ser mayores a 0';
 
-
-
-
+  return error;
+}
 
 export default function CheckoutForm() {
-    const [coins,setCoins] = useState(undefined);
-    const [input,setInput] = useState({cbu:'',value:0,name:''});
-    const [flag,setFlag] = useState(false);
-    const navigate = useNavigate()
+  const [coins, setCoins] = useState(0);
+  const [input, setInput] = useState({cbu:'', coins: 0, name:''});
+  const [flag, setFlag] = useState(false);
+  const [coinOptions, _] = useState([0, 10, 50, 100, 200, 500, 1000]);
+  const [error, setError] = useState({})
 
-    useEffect( () =>{
-      axios.get("http://localhost:3001/api/profile/coins",{withCredentials:true})
-      .then(data => {setCoins(data.data.coins);
-      if(!flag ){
-      setFlag(true)
-      }console.log(data)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/profile/coins", { withCredentials: true })
+      .then(data => {
+        setCoins(data.data.coins);
+        if (!flag) {
+          setFlag(true)
+        }
+        console.log(data)
       })
       .catch(error => console.log(error.response))
-    },[flag])
+  }, [flag])
 
+  function handleChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    function handleChange(e){
-      console.log(e.target.value)
-      setInput({
-        ...input,
-        [e.target.name]: e.target.value,
-      });
+  function handleSubmit(e) {
+    e.preventDefault();
+    const error = validate(input);
+    console.log(input)
+    console.log(error)
+    if (error.name || error.cbu || error.coins) {
+      setError(error);
+      return;
     }
 
-    function handleSubmit(e){
-      e.preventDefault();
-      const {name,cbu} = input;
-      if(name.length < 3) {
-        return alert('nombre invalido')
-      }else if (cbu.length !==  22) {
-        return alert('cbu incorrecto')
-      }
-      console.log(input,'inputhandle')
-      axios.post('http://localhost:3001/api/coins/sell',input,{withCredentials:true})
-      setInput ({cbu:'',value:0,name:''})
-    
-      navigate('/')
-     
-    }
-
-   
-    
-  
-
+    axios.post('http://localhost:3001/api/coins/sell', input ,{ withCredentials:true })
+    alert('Su solicitud esta siendo procesada');
+    setInput ({ cbu:'', coins: 0, name:''})
+    navigate('/')
+  }
 
   return ( 
     <Fragment>
-    <Box
-      paddingTop={'2%'}
-      sx={{ display: 'flex' }}
-      sx={{ mt: '15%' }}
-      sx={{ md: { xs: '20%', md: '40%', lg: '100%' } }}>
-      {(flag ) ? (
-      <div>
-        <FormControl   onSubmit={(e) => handleSubmit(e)} 
-          sx={{
-            width: 600,
-            height:'auto',
-            borderRadius: '5px',
-            backgroundColor: '#192A45',
-            borderColor:'#192A45',
-            color: '#357DED',
-          }}>
-          <h1 >RETIRA TUS MONEDAS</h1> 
-          <h3>( 1 moneda = $ 7 ) </h3>
-          <h3>Monedas Disponibles: {coins}</h3>
-          <Box sx={{ mt: '2rem' }}>
+      <Box
+        paddingTop={'2%'}
+        sx={{ display: 'flex' }}
+        sx={{ mt: '15%' }}
+        sx={{ md: { xs: '20%', md: '40%', lg: '100%' } }}>
+        {(flag ) ? (
+        <div>
+          <FormControl 
+            onSubmit={(e) => handleSubmit(e)} 
+            sx={{
+              width: 600,
+              height:'auto',
+              borderRadius: '5px',
+              backgroundColor: '#192A45',
+              borderColor:'#192A45',
+              color: '#357DED',
+            }}>
+            <Box sx={{ m: '10px', borderRadius: '10px', border: '2px solid white' }}>
+              <h1 >RETIRA TUS MONEDAS</h1> 
+              <h3>( 1 moneda = $ 7 ) </h3>
+              <h3>Monedas Disponibles: {coins}</h3>
+            </Box>
+            <Box sx={{ mt: '2rem' }}>
+              <div>
+                <FormControl>
+                  <Input 
+                    placeholder = 'NOMBRE TITULAR DE LA CUENTA' 
+                    sx = {{ 
+                      width :'32rem',
+                      justifyContent:'center',
+                      backgroundColor:'white'
+                    }}
+                    type = "text"
+                    value = {input.name}
+                    name = "name"
+                    onChange = {(e) => handleChange(e)}
+                  />
+                  {error.name && <p className='error'>{error.name}</p>}
+                </FormControl>
+              </div> 
+            </Box>
+            <Box sx={{ mt: '2rem' }}>
+              <div>
+                <Input 
+                  placeholder = 'CBU' 
+                  sx = {{ 
+                    width :'32rem',
+                    justifyContent:'center',
+                    backgroundColor:'white',
+                    textAlign: 'center'
+                  }}
+                  type = "text"
+                  value={input.cbu}
+                  name = "cbu"
+                  onChange = {(e) => handleChange(e)}
+                />
+                {error.cbu && <p className='error'>{error.cbu}</p>}
+              </div> 
+            </Box>         
+            <Box sx={{ mt: '2rem' }}>
+              <div>
+                { flag ?                    
+                        <select 
+                          style = {{
+                            width: 32 +'rem', 
+                            justifyContent:'center',
+                            backgroundColor:'white',
+                            textAlign:'center',
+                            height: 2 +'rem'
+                          }} 
+                          name="coins" 
+                          onChange = {handleChange} 
+                          value={input.coins}>
+                          {coinOptions.map(c => {
+                              if (c <= coins) {
+                                return <option key={c} value={c}> {c} </option>
+                              }
+                            })
+                          }
+                        </select>
+                      : null
+                    }
+                    {error.coins && <p className='error'>{error.coins}</p>}
+              </div> 
+            </Box>
             <div>
-            <Input placeholder='NOMBRE TITULAR DE LA CUENTA' sx={{width :'32rem',justifyContent:'center',backgroundColor:'white'}}
-              type="text"
-              value={input.name}
-              name="name"
-            onChange={(e) => handleChange(e)}
-            />
-          </div> 
-          </Box>
-          <Box sx={{ mt: '2rem' }}>
-            <div>
-            <Input placeholder='CBU' sx={{width :'32rem',justifyContent:'center',backgroundColor:'white',textAlign: 'center'}}
-              type="text"
-              value={input.cbu}
-              name="cbu"
-            onChange={(e) => handleChange(e)}
-            />
-          </div> 
-          </Box>         
-          <Box sx={{ mt: '2rem' }}>
-            <div>
-           { flag ?                    
-                    (coins >= 1000) ? (
-                    <select style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}}  name="value" onChange={handleChange} value={input.value}>
-                    <option value="0">Selecciona una opción</option>
-                    <option value="10">10</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="200">200</option>
-                    <option value="500">500</option>
-                    <option value="1000">1000</option>
-                    </select>) : 
-                    (coins >= 500) ? (
-                    <select style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}} name="value" onChange={handleChange} value={input.value}>
-                     <option disabled selected>Selecciona una opción</option>
-                    <option value="0">0</option>
-                    <option value="10">10</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="200">200</option>
-                    <option value="500">500</option>
-                    </select>
-                   ) : 
-                   (coins >= 200) ? (<select name="value" onChange={handleChange} value={input.value}>
-                   <option style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}} value="0">0</option>
-                   <option disabled selected>Selecciona una opción</option>
-                   <option value="10">10</option>
-                   <option value="50">50</option>
-                   <option value="100">100</option>
-                   <option value="200">200</option>
-                   </select>) :
-                   (coins >= 100) ? (
-                   <select style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}} name="value" onChange={handleChange} value={input.value}>
-                    <option disabled selected>Selecciona una opción</option>
-                   <option value="0">0</option>
-                   <option value="10">10</option>
-                   <option value="50">50</option>
-                   <option value="100">100</option>
-                   </select>) : 
-                   (coins >= 50) ? (
-                   <select style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}} name="value" onChange={handleChange} value={input.value}>
-                    <option disabled selected>Selecciona una opción</option>
-                   <option value="0">0</option>
-                   <option value="10">10</option>
-                   <option value="50">50</option>
-                   </select>) : 
-                   (coins >= 10) ? (<select style={{width: 32 +'rem', justifyContent:'center',backgroundColor:'white',textAlign:'center',height: 2 +'rem'}} name="value" onChange={handleChange} value={input.value}>
-                   <option value="0">0</option>
-                   <option value="10">10</option>
-                   </select>) : (<select name="value" onChange={handleChange} value={input.value}>
-                   <option disabled selected>Selecciona una opción</option>
-                    <option value="0">0</option>
-                    </select>) : null}
-          </div> 
-          </Box>
-          <div>
-            <Box sx={{ width: '100%', py: '1rem' }}>
-              <Button  onClick={(e) => handleSubmit(e)} size="small"  variant="contained">Enviar</Button></Box>
-            <Box sx={{ width: '100%', py: '0.2rem' }}>
+              <Box sx = {{ width: '100%', py: '1rem' }}>
+                <Button 
+                  onClick={(e) => handleSubmit(e)} 
+                  size="small" 
+                  variant="contained">
+                    Enviar
+                </Button>
               </Box>
-      
-          </div>
-        </FormControl>
-      </div>
-      ) : (<div>cargando</div>)}
-    </Box>
+              <Box sx={{ width: '100%', py: '0.2rem' }}>
+              </Box>
+            </div>
+          </FormControl>
+        </div>
+        ) : (<div>cargando</div>)}
+      </Box>
     </Fragment>
   )
 }
-            
-             
-
-
-            
-             
-
-
