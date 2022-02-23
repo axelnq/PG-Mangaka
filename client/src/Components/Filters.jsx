587a7ce059c244a6027a4d0bd484c634533124d1
Cintia Arce <cintiaayelenarce@gmail.com>
2022-02-23 12:00:59 -0300

    Filters new style

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -1,12 +1,17 @@
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import Stack from '@mui/material/Stack';
+import FormControl from '@mui/material/FormControl'
 import { styled } from "@mui/material/styles";
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuList from '@mui/material/MenuList';
 
-import { React, useEffect } from 'react'; // hooks
+import { React, useEffect, useState, useRef } from 'react'; // hooks
 import { useDispatch, useSelector } from 'react-redux'; // hooks
 import { orderMangas, filterMangasByAuthor, filterMangasByGenre, getAllMangas, paginado, getGenres, changeShow } from '../Actions';
 
@@ -41,21 +46,46 @@ const Filters = () => {
         dispatch(filterMangasByAuthor(e.target.value))
     }
 
-    let handleFilterGenre = (e) => {
-        e.preventDefault()
+    let handleFilterGenre = (option) => {
         if(show)dispatch(changeShow())
-        dispatch(filterMangasByGenre(e.target.value))
-        dispatch(paginado({page: 1, genre: e.target.value, order: filters.order}))
+        dispatch(filterMangasByGenre(option))
+        dispatch(paginado({page: 1, genre:option, order: filters.order}))
     }
+
+
 
     useEffect(() => {
         dispatch(getGenres())
     }, [])
+    
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef(null);
+    const [selectedIndex, setSelectedIndex] = useState(1);
+
+    
+      const handleClickFilterGenre = (event, index, option) => {
+        setSelectedIndex(index);
+        setOpen(false);
+        handleFilterGenre(option)
+      };
+    
+      const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+      };
+    
+      const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+          return;
+        }
+    
+        setOpen(false);
+      };
 
     const StackContainer = styled(Stack)`
         width: 100%;
         display: flex;
         flex-direction: row;
+        justify-content: space-evenly;
         overflow-x: scroll;
         scrollbar-width: thin;
         ::-webkit-scrollbar {
@@ -65,53 +95,227 @@ const Filters = () => {
             width: 100%
         }
     `
+    const Buttons = styled(Button)`
+        margin: 1rem;
+        padding: 1rem;
+        background-color:#357DED;
+        color:#fff;
+    `
+    const SelectContainer = styled(FormControl)`
+        margin: 1rem;
+        background-color: #357DED;
+        border-radius:10%;
+        heigth: 1rem;
+        label {
+            color:#fff;
+            padding: 0 1rem;
+        } 
+        div {
+            
+            margin: 0 1.5rem;
+            padding: 0 1.5rem;
+            
+            div {
+
+            }
+            input {
+            }
+        }
+    `
 
     return (
             <StackContainer sx={{ backgroundColor: '#192A45' }}  >
-                <Button variant="contained" onClick={handleRecomendation} sx={{ color: '#357DED', px:'60px',width: '150px', height: '3.4rem', backgroundColor: '#000', my: '1rem', mx: '1rem', borderRadius: 2 }}>Para Vos</Button>
-                <FormControl sx={{  backgroundColor: '#000',width: '150px', my: '1rem', mx: '1rem', borderRadius: 2, px:'60px' }}>
-                    <InputLabel id="demo-simple-select-label" sx={{ color: '#357DED', width: '150px' }}>GÉNEROS POPULARES</InputLabel>
+                <Buttons 
+                    variant="outlined"
+                    onClick={handleRecomendation} 
+                >
+                    Para Vos
+                </Buttons>
+                {/* GENEROS */}
+                <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button">
+                    <Button >{genres[selectedIndex]}</Button>
+                    <Button
+                    size="small"
+                    aria-controls={open ? 'split-button-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-label="select merge strategy"
+                    aria-haspopup="menu"
+                    onClick={handleToggle}
+                    >
+                    <ArrowDropDownIcon />
+                    </Button>
+                </ButtonGroup>
+                <Popper
+                    open={open}
+                    anchorEl={anchorRef.current}
+                    role={undefined}
+                    transition
+                    disablePortal
+                >
+                    {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                        transformOrigin:
+                            placement === 'bottom' ? 'center top' : 'center bottom',
+                        }}
+                    >
+                        <Paper>
+                        <ClickAwayListener onClickAway={handleClose}>
+                            <MenuList id="split-button-menu">
+                            {genres.map((option, index) => (
+                                <MenuItem
+                                key={option}
+                                selected={index === selectedIndex}
+                                value={option}
+                                onClick={(event) => handleClickFilterGenre(event, index, option)}
+                                >
+                                {option}
+                                </MenuItem>
+                            ))}
+                            </MenuList>
+                        </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                    )}
+                </Popper>
+                {/* AUTORES */}
+                <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button">
+                    <Button >Admin</Button>
+                    <Button
+                    size="small"
+                    aria-controls={open ? 'split-button-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-label="select merge strategy"
+                    aria-haspopup="menu"
+                    onClick={handleToggle}
+                    >
+                    <ArrowDropDownIcon />
+                    </Button>
+                </ButtonGroup>
+                <Popper
+                    open={open}
+                    anchorEl={anchorRef.current}
+                    transition
+                    disablePortal
+                >
+                    {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                        transformOrigin:
+                            placement === 'bottom' ? 'center top' : 'center bottom',
+                        }}
+                    >
+                        <Paper>
+                        <ClickAwayListener onClickAway={handleClose}>
+                            <MenuList id="split-button-menu">
+                                <MenuItem
+                                value=''
+                                onClick={(event) => handleFilterAuthor(event)}
+                                >''
+                                </MenuItem>
+                                <MenuItem
+                                value='Admin'
+                                onClick={(event) => handleFilterAuthor(event)}
+                                >'Admin'
+                                </MenuItem>
+                            </MenuList>
+                        </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                    )}
+                </Popper>
+                {/* <SelectContainer sx={{mx:'1rem'}} variant="standard">
+                    <InputLabel 
+                       id="demo-customized-select-label"
+                    >
+                        AUTORES
+                    </InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Géneros populares"
-                        sx={{ color: '#357DED', backgroundColor: '#000' }}
-                        onChange={handleFilterGenre}
-                    ><MenuItem value={''} sx={{ color: '#357DED' }}>Default</MenuItem>
-                        {
-                            genres && genres.map((g, i) => <MenuItem key={i} value={g} sx={{ color: '#357DED' }}>{g}</MenuItem>)
-                        }
-                    </Select>
-                </FormControl>
-                <FormControl sx={{  backgroundColor: '#000', px:'60px',width: '150px', my: '1rem', mx: '1rem', borderRadius: 2 }}>
-                    <InputLabel id="demo-simple-select-label" sx={{ width: '150px',color: '#357DED' }}>AUTORES</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
+                        labelId="demo-customized-select-label"
+                        id="demo-customized-select"
                         label="Autores"
-                        sx={{ color: '#357DED' }}
                         onChange={handleFilterAuthor}
                     >
-                        <MenuItem value={''} sx={{ color: '#357DED' }}></MenuItem>
-                        <MenuItem value={'Admin'} sx={{ color: '#357DED' }}>ADMIN</MenuItem>
+                        <MenuItem value={''} ></MenuItem>
+                        <MenuItem value={'Admin'} >ADMIN</MenuItem>
                     </Select>
-                </FormControl>
-                <FormControl sx={{  backgroundColor: '#000', px:'60px', width: '150px', my: '1rem', mx: '1rem', borderRadius: 2 }}>
-                    <InputLabel id="demo-simple-select-label" sx={{ color: '#357DED', width: '150px' }}>ORDEN</InputLabel>
+                </SelectContainer> */}
+                <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button">
+                    <Button >Admin</Button>
+                    <Button
+                    size="small"
+                    aria-controls={open ? 'split-button-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-label="select merge strategy"
+                    aria-haspopup="menu"
+                    onClick={handleToggle}
+                    >
+                    <ArrowDropDownIcon />
+                    </Button>
+                </ButtonGroup>
+                <Popper
+                    open={open}
+                    anchorEl={anchorRef.current}
+                    transition
+                    disablePortal
+                >
+                    {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                        transformOrigin:
+                            placement === 'bottom' ? 'center top' : 'center bottom',
+                        }}
+                    >
+                        <Paper>
+                        <ClickAwayListener onClickAway={handleClose}>
+                            <MenuList id="split-button-menu">
+                                <MenuItem
+                                value=''
+                                onClick={(event) => handleOrder(event)}
+                                >''
+                                </MenuItem>
+                                <MenuItem
+                                value='Admin'
+                                onClick={(event) => handleOrder(event)}
+                                >'A-Z'
+                                </MenuItem>
+                                <MenuItem
+                                value='Admin'
+                                onClick={(event) => handleOrder(event)}
+                                >'Z-A'
+                                </MenuItem>
+                            </MenuList>
+                        </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                    )}
+                </Popper>
+                {/* <SelectContainer sx={{mx:'1rem'}} variant="standard">
+                    <InputLabel 
+                        id="demo-customized-select-label"
+                    >
+                        ORDEN
+                    </InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
+                        labelId="demo-customized-select-label"
+                        id="demo-customized-select"
                         label="Autores"
-                        sx={{ color: '#357DED' }}
                         onChange={handleOrder}
                     >
-                        <MenuItem value={''} sx={{ color: '#357DED' }}>Default</MenuItem>
-                        <MenuItem value={'asc'} sx={{ color: '#357DED' }}>A-Z</MenuItem>
-                        <MenuItem value={'desc'} sx={{ color: '#357DED' }}>Z-A</MenuItem>
-                        {/* <MenuItem value={'createdAt'} sx={{ color: '#357DED' }}>NUEVOS</MenuItem> */}
+                        <MenuItem value={''} >Default</MenuItem>
+                        <MenuItem value={'asc'} >A-Z</MenuItem>
+                        <MenuItem value={'desc'} >Z-A</MenuItem>
                     </Select>
-                </FormControl>
-                <Button variant="contained" onClick={handleGetAll} sx={{ color: '#357DED', px:'60px', width: '150px',  height: '3.4rem', backgroundColor: '#000', my: '1rem', mx: '1rem', borderRadius: 2 }}>Todos</Button>
+                </SelectContainer> */}
+                <Buttons 
+                    variant="outlined"
+                    onClick={handleGetAll} 
+                >
+                    Todos
+                </Buttons>
             </StackContainer>
 
     )
